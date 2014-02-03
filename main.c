eb46e6d7966cc2b7a5ed4191ba225d543143c11b
Jilang_Miao <jlmiao@mit.edu>
2014-02-03 16:02:23 -0500

    test program in c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#define ROW 2
+#define COL 4
+
+void main(){
+  double vec0[COL+COL];
+  double* vec1;
+  double* vec2;
+  double array[ROW][COL];
+  int i,j, nrow, ncol;
+  vec1 = vec0;
+  vec2 = vec0 + COL;
+  nrow = ROW;
+  ncol = COL;
+  for(i=0; i<ROW; i++)
+    for(j=0; j<COL; j++)
+      array[i][j]=210 + 10*i+j;
+  
+  for(i=0; i<COL; i++){
+    vec1[i]=10 + i;
+    vec2[i]=20 + i;
+  }
+  writeh5_nx2_t_("t2d.h5", "one 2D", (double*)array[0], &nrow, &ncol);
+  writeh5_nx2_t_("t2v.h5", "two 1D", (double*)vec0,     &nrow, &ncol);
+
+}
