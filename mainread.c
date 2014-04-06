81c797440a188c984f7c5b700e45419d641e5d39
Jilang Miao 缪佶朗 <jlmiao@mit.edu>
2014-04-06 11:50:54 -0400

    probably i forgot to update

--- mainread.c
+++ mainread.c
@@ -5,6 +5,11 @@
 
 void main(){
   int * array = malloc(5*6*4*sizeof(int));
+  double * xy = malloc(50*2*sizeof(double)); 
   readh5_("SDS.h5", "/IntArray", array);
-  printf("array(2,1,2)=%2d", array[6*4*2+4*1+2]);
+  printf("array(2,1,2)=%2d\n", array[6*4*2+4*1+2]);
+
+  readh5_("results.h5","/xypair",xy);
+  printf("xy(10,1)=%g\n", xy[10]);
 }
+
